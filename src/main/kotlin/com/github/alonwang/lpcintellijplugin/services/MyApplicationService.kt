2ee832df547f54214496073a85ab65694c8c36c3
GitHub Action <action@github.com>
2021-11-30 15:40:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alonwang/lpcintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alonwang.lpcintellijplugin.services
+
+import com.github.alonwang.lpcintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/alonwang/lpcintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alonwang/lpcintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alonwang.lpcintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alonwang.lpcintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
